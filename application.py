7177146e373d2d53f38c32cf60e91a8b677cd6a7
devynqualls <58799637+devynqualls@users.noreply.github.com>
2021-04-23 14:51:22 -0400

    Add files via upload

---
+++ application.py
@@ -0,0 +1,25 @@
+from flask import Flask
+app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Food(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.string(80), unique=True, nullable=False)
+    description = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.name} - {self.description}"
+
+@app.route('/')
+def index():
+    return 'Hello!'
+
+@app.route('/food')
+def get_food():
+    
+
+Food.query.all()
